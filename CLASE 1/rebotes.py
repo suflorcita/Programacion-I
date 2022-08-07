d2bb5cfad73dc012b8a69c8bd14cc6aa9f84b40e
Sol C <solcataldo98@gmail.com>
2022-08-06 23:03:27 -0300

    Versión final ejercicio rebotes

--- CLASE 1/rebotes.py
+++ CLASE 1/rebotes.py
@@ -1,13 +1,7 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Wed Aug  3 12:51:33 2022
-
-@author: infolab
-"""
+# Alumna: Sol Ayelen Cataldo 
 
 altura = 100
 for i in range(10):
     altura = 3/5 * altura
-    print(i+1, round(altura,2))
+    print(i+1, round(altura,4))
     
