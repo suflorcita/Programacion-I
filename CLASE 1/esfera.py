bb165ae0cadba11d0d22b3ee553c44f595f575f2
Sol C <solcataldo98@gmail.com>
2022-08-06 22:29:40 -0300

    Versión final ejercicio esfera

---
+++ CLASE 1/esfera.py
@@ -0,0 +1,12 @@
+
+# Alumna: Sol Ayelen Cataldo 
+
+import math 
+
+radio = float(input("Ingrese el radio de una esfera: "))
+volumen = 4/3 * math.pi * radio ** 3
+
+print("El volumen de la esfera es", volumen)
+
+# input: 6
+# output: "El volumen de la esfera es 904.7786842338603" 
