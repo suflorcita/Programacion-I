b6360841fbcfd738e68631f4b7a2bdb67ef4bb84
Sol C <solcataldo98@gmail.com>
2022-08-06 22:00:04 -0300

    Versión final

--- CLASE 1/saludo.py
+++ CLASE 1/saludo.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-"""
-Created on Wed Aug  3 12:55:22 2022
 
-@author: infolab
-"""
+# Alumna: Sol Ayelen Cataldo 
 
 nombre = input("Ingrese su nombre: ")
-print("Hola, ", nombre)
+print("Hola,", nombre)
+
+# input: Juana 
+# output: "Hola, Juana "
