a5089f502432ffe97a2661dde56060bfdc1200c4
Sol C <solcataldo98@gmail.com>
2022-08-07 00:44:55 -0300

    Modificacion al ejercicio geringoso

--- CLASE 1/geringoso.py
+++ CLASE 1/geringoso.py
@@ -1,10 +1,4 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Wed Aug  3 13:27:15 2022
-
-@author: infolab
-"""
+# Alumna: Sol Ayelén Cataldo 
 
 cadena = 'Geringoso'
 capadepenapa = ''
