e31fc4a6301dfc61d4e51d1087138a31c567be77
Sol C <solcataldo98@gmail.com>
2022-09-01 07:32:08 -0300

    Ejercicios para aprender a debbugear

---
+++ CLASE 5/debug.py
@@ -0,0 +1,14 @@
+def invertir_lista(lista):
+    '''Recibe una lista L y la develve invertida.'''
+    invertida = []
+    i = len(lista)
+    while i > 0:    # tomo el último elemento 
+        i = i-1
+        invertida.append (lista.pop(i))  # lista.pop(i) elimina el ultimo elemento 
+    return invertida
+
+l = [1, 2, 3, 4, 5]    
+m = invertir_lista(l)
+print(f'Entrada {l}, Salida: {m}')
+
+
