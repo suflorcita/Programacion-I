5151da7eb425641c8140115f9227c371cc05a476
Sol C <solcataldo98@gmail.com>
2022-09-21 11:30:31 -0300

    Ejercicio Generala

--- CLASE 6/generala.py
+++ CLASE 6/generala.py
@@ -1,13 +1,18 @@
 import random 
+from collections import Counter 
 
-def tirar(): 
+def tirar(N): 
+    '''Devuelve una lista con N dados generados aleatoriamente'''
     tirada = []
 
-    for i in range(5): 
+    for i in range(N): 
         tirada.append(random.randint(1, 6))
     return tirada
 
+
+
 def es_generala(tirada):
+    '''Devuelve True si los cinco dados son iguales'''
     conj_tirada = set(tirada) 
     if len(conj_tirada) == 1: 
         return True 
@@ -15,13 +20,34 @@ def es_generala(tirada):
 
 
 def prob_generala(N):   
-    G = [sum([es_generala(i) for i in range(N )])]
-    return prob 
+    '''Devuelve la probabilidad de obtener una generala al finalizar una mano de tres tiradas 
+        para una simulación de N elementos'''
+        
+    suma = 0 
+    for i in range(N): 
+        # empiezo el juego 
+        generala = False
+        n = 0 # dados que tengo guardado 
+        
+        for tirada in range(3):
+            dados_tirada = tirar(5 - n) # tiro los dados que me quedan por tirar 
+            conteo = Counter(dados_tirada)    # cuento los valores dentro de la tirada
+            
+            if tirada == 0: # en la primera tirada elijo un valor 
+                valor_guardado = conteo.most_common(1)[0][0] # numero del dado guardado 
+                n = conteo.most_common(1)[0][1] # cantidad de veces que salio 
 
-N = 1
-G = sum([es_generala(tirar()) for i in range(N)])
-prob = G/N # probabilidad de tener generala servida 
-print(f'Tiré {N} veces, de las cuales {G} saqué generala servida.')
-print(f'Podemos estimar la probabilidad de sacar generala servida mediante {prob:.6f}.')
+            else: # en las otras dos tiradas compara 
+                if valor_guardado in conteo: 
+                    n += conteo[valor_guardado] 
+        
+            if n == 5: generala = True
+        # termino el juego 
+
+        suma += generala 
+    
+    return suma/N
 
 
+
+print(prob_generala(10000))
